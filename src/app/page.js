1c6442793e26c352ee9f37949599b729fbd7d6a2
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-14 13:20:52 -0500

    Banner Terminado con imagen x y tamaño de las letras para utilizar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
+import BannerHome from "@/components/Banners/BannerHome/BannerHome";
 import { createIconsTypes } from "@/utils/createIcons";
 
 export default function Home() {
   // createIconsTypes();
   return (
     <main >
-      <h1>Hola</h1>
+      <BannerHome/>
     </main>
-
   )
 }
