ef876c55b81b5685b13b3aa3d3f40a7104cfffdf
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-14 16:27:07 -0500

    Objetivos terminados solo falta los iconos de acuerdo

--- src/components/Card/CardGoals.jsx
+++ src/components/Card/CardGoals.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import IconText from '../IconText';;
 
-const CardGoals = ({desciption, icon, texticon}) => {
+const CardGoals = ({description, icon, textIcon}) => {
   return (
-      <div className="w-[272px] h-[250px]">
-            <IconText icon={icon} text={texticon}/>
-            <div className='w-[272px] mt-6 h-[173px] bg-purple border-none rounded-md border flex items-center justify-start p-4 h' style={{ boxShadow: '0px 10px 10px -5px rgba(0, 0, 0, 0.5)' }}>
-                <h3 className='text-white'>
-                    {desciption}
+      <div className="w-[272px] h-[250px] mb-10">
+            <IconText icon={icon} text={textIcon}/>
+            <div className='w-[272px] mt-6 h-[173px] bg-purple border-none rounded-md border flex items-center justify-start p-4' style={{ boxShadow: '0px 10px 10px -5px rgba(0, 0, 0, 0.5)' }}>
+                <h3 className='text-white text-center' >
+                {description}
                 </h3>
             </div>
       </div>  
---
+++ src/components/Containers/ContainerGoals.jsx
@@ -0,0 +1,36 @@
+// CardGoalsContainer.js
+import React from 'react';
+import CardGoals from '../Card/CardGoals';
+
+const CardGoalsContainer = () => {
+  return (
+    <div className='pt-[60px] pb-[60px]'> 
+        <h1 className='text-purple mb-[40px]'>Nuestros Objetivos</h1>
+        <div className="flex flex-wrap justify-between">
+            <CardGoals
+                description="Sobre la condición de Valentino y tantos niños excluidos del sistema."
+                icon="Facebook"
+                textIcon="CONCIENTIZAR"
+            />
+            <CardGoals
+                description="Más opciones de ortopedia y mejores terapias con Tucumán."
+                icon="Facebook"
+                textIcon="CONECTAR"
+            />
+            <CardGoals
+                description="A terapeutas y médicos con nuevas tecnologías y terapias"
+                icon="Facebook"
+                textIcon="CAPACITAR"
+            />
+            <CardGoals
+                description="Valorar y respetar las diferencias porque no hay una sola forma de ser para ser valioso, interesante, querible o exitoso."
+                icon="Facebook"
+                textIcon="EMPATIZAR"
+            />
+        </div>
+    </div>
+  );
+};
+
+export default CardGoalsContainer;
+
