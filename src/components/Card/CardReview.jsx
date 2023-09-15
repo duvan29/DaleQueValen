eb177451e81595d88065561a5856ff963167c1f3
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-15 09:14:52 -0500

    cracion de archivos

---
+++ src/components/Card/CardReview.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CardReview = () => {
+  return (
+      <div>
+        <div>
+          <div className='w-[295px] h-[420px] bg-yellow opacity-20'>
+
+          </div>
+        </div>
+      </div>  
+  );
+};
+
+export default CardReview;
