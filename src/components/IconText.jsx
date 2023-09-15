8f6adb485483fc05a52df641d70e498fbb3644f9
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-15 08:02:36 -0500

    Solucion de un bug

--- src/components/IconText.jsx
+++ src/components/IconText.jsx
@@ -3,11 +3,11 @@ import Icon from "@/assets/icons/icon"; // Importa de manera estática
 
 export default function IconText({ icon, text }) {
   return (
-    <p className="flex justify-center items-center">
+    <div className="flex justify-center items-center">
       <div className="h-14 w-14">
         <Icon icon={icon}/>
       </div>
       <h2 className="text-purple ml-4">{text}</h2>
-    </p>
+    </div>
   );
 }
