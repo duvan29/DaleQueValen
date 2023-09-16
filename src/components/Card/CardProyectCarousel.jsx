c5bba6e9ffd9262d9acdf7be80e7ccd7e919fad3
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-16 08:57:54 -0500

    Botton arreglado

--- src/components/Card/CardProyectCarousel.jsx
+++ src/components/Card/CardProyectCarousel.jsx
@@ -10,9 +10,9 @@ const CardProyectCarousel = ({title, description, image}) => {
       <div className='w-[40%] h-full flex flex-col justify-center relative'>
         <h1 className='text-black mb-[20px]'>{title}</h1>
         <p className='text-black mb-[20px] max-w-[500px]'>{description}</p>
-        <div className='absolute bottom-0 right-0 m-8'>
-          <Link href='/proyectos' className="w-[175px]">
-                <ReusableButton color="white" border='orange' width="full">
+        <div className='absolute bottom-0 right-0 m-8 w-[175px]'>
+          <Link href='/proyectos'>
+                <ReusableButton color="white" border='orange' text='orange' width="full">
                   Ver más
                 </ReusableButton>
           </Link>
