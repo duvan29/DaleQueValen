1f31c6e3f8961e8b844e7b687c0a56610e6f5807
EstebanGaMi <stevegaray1234@gmail.com>
2023-09-18 18:28:45 +0100

    iconos

--- src/app/Foooter/Footer.jsx
+++ src/app/Foooter/Footer.jsx
@@ -1,77 +1,81 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from "next/image";
-import styles from '@/app/Nav/Navigation.module.css';
 import { Images } from "@/assets/img";
-import Facebook from '@/assets/icons/Facebook';
-import Instagram from '@/assets/icons/Instagram';
-import Whatsapp from '@/assets/icons/Whatsapp';
-import Youtube from '@/assets/icons/Youtube';
-import Linkedin from '@/assets/icons/Linkedin';
-import Mail from '@/assets/icons/Mail';
-
-
+import CardGoals from '@/assets/icons/icon';
 
 
 const Footer = () => {
   return (
-    <footer className='bg-[#a9d730] text-white h-1/2 w-full flex md:flex-row flex-col justify-around p-10 bg-opacity-90 text-center items-center'>
-      <div className='p-5'>
+    <footer className='bg-[#a9d730] text-white h-1/2 p-5 w-full flex md:flex-row flex-col justify-between text-center items-center'>
+      <div className='mx-32'>
         <ul>
-          <div className='flex gap-6 pb-5'>
+          <div className='flex gap-6'>
             <Link
               className='w-10 hover:text-[#8e83bc]'
               href={"https://www.facebook.com/"}
               target='_blank'>
-              <Facebook />
+              <CardGoals
+                icon="Facebook"
+              />
             </Link>
             <Link
               className='w-10 hover:text-[#8e83bc]'
               href={"https://www.youtube.com/"}
               target='_blank'>
-              <Youtube />
+                <CardGoals
+                icon="Youtube"
+              />
             </Link>
             <Link
               className='w-10 hover:text-[#8e83bc]'
               href={"https://www.instagram.com/dalequevalen/"}
               target='_blank'>
-              <Instagram />
+                <CardGoals
+                icon="Instagram"
+              />
             </Link>
             <Link
               className='w-10 hover:text-[#8e83bc]'
               href={"https://www.linkedin.com/"}
               target='_blank'>
-              <Linkedin />
+                <CardGoals
+                icon="Linkedin"
+              />
             </Link>
           </div>
         </ul>
       </div>
       <div>
         <div>
-          <Image className={styles.logo} src={Images.logos.LogoBlack} alt="DaleQueValen Logo" priority />
+          <Image className='w-60' src={Images.logos.LogoBlack} alt="DaleQueValen Logo" priority />
         </div>
         <p>
           ©️Copyright
         </p>
       </div>
-      <div className='flex gap-6 pt-5 flex-col'>
+      <div className='flex gap-4 flex-col mx-32'>
         <div className='flex'>
           <Link
-            className=' w-10 hover:text-[#8e83bc] flex mx-2' 
+            className=' w-10 hover:text-[#8e83bc] flex mx-2'
             href={"https://gmail.com/"}
             target='_blank'>
-            <Mail />
+              <CardGoals
+                icon="Mail"
+              />
           </Link>
-          <p className='gap-6 mt-2'>
+          <p className='gap-4 mt-2'>
             dalequevalen@gmail.com
           </p>
         </div>
         <div className='flex'>
-        <Link
+          <Link
             className=' w-10 hover:text-[#8e83bc] flex mx-2'
             href={"https://web.whatsapp.com/"}
             target='_blank'>
-            <Whatsapp />
+             <CardGoals
+                icon="Whatsapp"
+              />
           </Link>
           <p className='gap-6 mt-2'>
             +54-2612425949
--- src/types/icons.d.ts
+++ src/types/icons.d.ts
@@ -1,1 +1,1 @@
-export type IconTypes = | 'Eye' | 'Facebook' | 'Heart' | 'icon' | 'Instagram' | 'Pc' | 'quotations' | 'Share' | 'Whatsapp' 
+export type IconTypes = | 'Eye' | 'Facebook' | 'Heart' | 'icon' | 'Instagram' | 'Pc' | 'quotations' | 'Share' | 'Whatsapp' | 'Mail' | 'Youtube' | 'Linkedin'
