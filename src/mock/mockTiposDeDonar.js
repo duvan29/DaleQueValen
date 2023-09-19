6707f8f132d389eef3bcad3ef12f27dc4f434da5
Candeb <candebustos084@gmail.com>
2023-09-19 11:59:43 -0300

    se agrega mock de tipos de donar

---
+++ src/mock/mockTiposDeDonar.js
@@ -0,0 +1,36 @@
+import { Images } from '@/assets/img';
+
+const tiposDeDonar = [
+  {
+    id: '1',
+    title: 'donar dinero',
+    description:
+      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
+    boton: 'Donar',
+    Image: Images.logos.LogoBrown,
+  },
+  {
+    id: '2',
+    title: 'ser voluntario',
+    description:
+      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
+    boton: 'Colaborar',
+    Image: Images.logos.LogoPink,
+  },
+  {
+    id: '3',
+    title: 'colaborar con equipamiento, insumos o medicamentos',
+    description:
+      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
+    boton: 'Colaborar',
+    Image: Images.logos.LogoPurple,
+  },
+  {
+    id: '4',
+    title: 'asociarte como familia',
+    description:
+      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
+    boton: 'Asociarme',
+    Image: Images.logos.LogoBlackWhite,
+  },
+];
