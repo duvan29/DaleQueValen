5d7e5443a6a1ae7657ceacae871d3c1549e74e65
Agustin <agustinperea5@gmail.com>
2023-09-19 15:14:17 -0300

    Terminado linea del tiempo

--- src/app/conocenos/page.jsx
+++ src/app/conocenos/page.jsx
@@ -1,11 +1,14 @@
 import ContainerMisionCarousel from "@/components/Containers/ContainerMision";
 import mision from "@/mock/mockMisionVisionValores";
 
+import ContainerTimeline from "@/components/Containers/ContainerTimeline";
+
 export default function Conocenos() {
     return(
         <main>
             <div>Conocenos</div>
             <ContainerMisionCarousel mision={mision}/>
+            <ContainerTimeline></ContainerTimeline>
         </main>
     )
 }
