6b64888206448542d1c4f4626cbb702a0523530b
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-20 17:36:16 -0500

    Cambios

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -4,6 +4,11 @@ export default function Proyectos() {
   return (
     <main>
       <SectionTitle bgColor="orange-50" title="proyectos" />
+      <div className='h-[700px] flex justify-center items-center text-center'>
+        <h1 className='text-orange'>
+          Estamos Trabajando la seccion de Proyectos
+        </h1>
+      </div>
     </main>
   );
 }
