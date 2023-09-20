de01989774599fedc9845117de6feda5ce92345b
Agustin <agustinperea5@gmail.com>
2023-09-20 14:59:28 -0300

    Corregido sticky, correccion de algunos estilos

--- src/components/ReusableButton/ReusableButton.jsx
+++ src/components/ReusableButton/ReusableButton.jsx
@@ -2,7 +2,11 @@
 
 
 export default function ReusableButton({color,border,width,text,children,isActive}){
-    const tailwindClass=`bg-${isActive ? color:`${color}-50`}  font-bold w-${width} h-16 px-4 py-3 hover:bg-${color} transition-all duration-500 my-8 ease-in-out rounded font-semibold flex items-center justify-center border-2 border-${border} text-${text ? text:'white'}`;
+    
+    const tailwindClass=` ${isActive ? ` bg-${color} `:` bg-green-50 `}  font-bold w-${width} h-16 px-4 py-3 hover:bg-green transition-all duration-500 my-8 ease-in-out font-bold flex items-center justify-center border-4 border-green rounded-xl text-${text ? text:'white'} text-xl`;
+    
+    // const tailwindClass=`bg-${isActive ? color:`${color}-50`}  font-bold w-${width} h-16 px-4 py-3 hover:bg-${color} transition-all duration-500 my-8 ease-in-out rounded font-semibold flex items-center justify-center border-2 border-${border} text-${text ? text:'white'}`;
+    // const tailwindClass2=`bg-${isActive ? `${color}`:`${color}-50`}  font-bold w-${width} h-16 px-4 py-3 hover:${'bg-'+color} transition-all duration-500 my-8 ease-in-out rounded font-semibold flex items-center justify-center border-2 border-${border} text-${text ? text:'white'}`;
     
 
     return(
