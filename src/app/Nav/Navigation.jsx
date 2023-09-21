dd5e5ccee2f7f610a8d975cbf342193e6848848d
Agustin <agustinperea5@gmail.com>
2023-09-21 11:16:48 -0300

    Agregado opacidad de menu y asignado clases fijas para botones menu

--- src/app/Nav/Navigation.jsx
+++ src/app/Nav/Navigation.jsx
@@ -34,7 +34,7 @@ export default function Navigation() {
 
   return (
     // <div className={`${pathname=='/' ? '':'sticky top-0 z-50 bg-white'} pr-[40px] pl-[40px] absolute left-0 right-0`}>
-    <div className={`${pathname=='/' ? 'pr-[40px] pl-[40px] absolute left-0 right-0':'fixed top-0 left-0 right-0 z-50 bg-white-50 px-[45px]'} `}>
+    <div className={`${pathname=='/' ? 'pr-[40px] pl-[40px] absolute left-0 right-0':'fixed top-0 left-0 right-0 z-50 bg-white-50 backdrop-blur-sm px-[45px]'} `}>
       <header className="flex items-center justify-between">
         <Link href="/" className={`${pathname=='/' ? 'w-[370px] ml-[45px]':'w-[185px]'} h-auto`}>
           <Image className="w-full h-full aspect-ratio-16/9" src={pathname=='/'? Images.logos.LogoWhite:Images.logos.LogoGreen} alt="DaleQueValen Logo" priority />
@@ -43,14 +43,14 @@ export default function Navigation() {
           <div className="w-full flex justify-around">
             {links.map(({ label, route }) => (
               <Link key={route} href={route} className="w-[175px] mr-[30px]">
-                <ReusableButton color='green' border='green' width="full" isActive={pathname == route}>
+                <ReusableButton  isActive={pathname == route}>
                 {label}
                 </ReusableButton>
               </Link>
             ))
             }
             <Link href="#contacto" className="w-[175px]" >
-              <ReusableButton color='green' border='green' width="full">
+              <ReusableButton>
                 Contacto
               </ReusableButton>
             </Link>
