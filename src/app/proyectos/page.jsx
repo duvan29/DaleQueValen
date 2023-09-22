4609c3197553974892b3123790a3b2e01f4b1b34
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-22 08:48:29 -0500

    Cambios

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -1,9 +1,10 @@
-import SectionTitle from '@/components/SectionTitle/SectionTitle';
+import SectionTitle from "@/components/SectionTitle/SectionTitle";
+
 
 export default function Proyectos() {
   return (
     <main>
-      <SectionTitle bgColor="orange-50" title="proyectos" />
+      <SectionTitle bgColor="orange" className='bg-orange' title="Proyectos" />
       <div className='h-[700px] flex justify-center items-center text-center'>
         <h1 className='text-orange'>
           Estamos Trabajando la seccion de Proyectos
