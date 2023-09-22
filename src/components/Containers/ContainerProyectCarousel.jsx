affddf0155b6f2da3e41d7ea8e9d8284cac20f6b
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-09-21 20:02:37 -0500

    Button cambiado

--- src/components/Containers/ContainerProyectCarousel.jsx
+++ src/components/Containers/ContainerProyectCarousel.jsx
@@ -13,7 +13,7 @@ const ContainerProyectCarousel = ({ proyects }) => {
         </div>
         <div className='md:flex md:justify-end md:w-[50%] ms:hidden'>
           <Link href='/sumate' className="w-[275px]">
-              <button className='bg-orange-20 w-full h-16 px-4 py-3 hover:bg-orange transition-all duration-500 ease-in-out flex items-center justify-center border-2 border-orange text-gray rounded-xl'>
+              <button className='bg-orange-20 w-full h-16 px-4 py-3 hover:bg-orange transition-all duration-500 ease-in-out flex items-center justify-center border-2 border-orange text-gray hover:text-white rounded-xl'>
                 <h6>Quiero Colaborar</h6>
               </button>
           </Link>
