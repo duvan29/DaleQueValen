7986fcb26efcfbf4f3687bf1bcf28e166cce10cb
Agustin <agustinperea5@gmail.com>
2023-09-25 17:16:00 -0300

    Agregada funcion tocar afuera del menu y cerrarlo. Agregado links active

--- src/app/Nav/NavResponsive.jsx
+++ src/app/Nav/NavResponsive.jsx
@@ -2,14 +2,15 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { usePathname } from "next/navigation";
 import { Images } from "@/assets/img"
 import links from "@/mock/mockNavLinks"
 import Icon from "@/assets/icons/icon"
 
 export default function NavResponsive() {
     const [isOpen, setIsOpen] = useState(false);
-
+    
     const toggleMenu = ()=>{
         setIsOpen(!isOpen)
     }
@@ -18,14 +19,31 @@ export default function NavResponsive() {
         isOpen ? setIsOpen(!isOpen) : '';
     }
 
+    const menuRef = useRef(null);
     
-      
+    const handleClickOut = () =>{
+        if(menuRef.current && !menuRef.current.contains(event.target)){
+            setIsOpen(false);
+        }
+    }
+    
+    const pathname = usePathname();
+
+    useEffect(()=>{
+        document.addEventListener('mousedown',handleClickOut);
+        return ()=>{
+            document.removeEventListener('mousedown',handleClickOut);
+        }
+    })
+
+
+
     return(
         <div className="fixed top-0 left-0 z-50 h-[80px] w-screen flex justify-between items-center bg-green p-4 mt-0 lg:hidden">
-            <div className={`fixed top-0 left-0 z-60 h-[100%] ${isOpen ? 'w-[60%] ':'w-0' }  bg-white transition-width duration-300 ease-in-out flex flex-col justify-between lg:hidden`}>
-               <div className={` flex flex-col border-t-2 mt-[80px] text-[14px] ${isOpen ? '':'hidden'}`}>
+            <div ref={menuRef} className={`fixed top-0 left-0 z-60 h-[100%] ${isOpen ? 'w-[60%] ':'w-0' }  bg-white transition-width duration-300 ease-in-out flex flex-col justify-between lg:hidden`}>
+               <div className={` flex flex-col border-t-2 mt-[80px] text-[14px] px-2 ${isOpen ? '':'hidden'}`}>
                     {links.map(({ label, route }) => (
-                    <Link onClick={toggleMenuLogo} key={route} href={route} className="w-full mr-[30px] flex justify-between px-6 py-4">
+                    <Link onClick={toggleMenuLogo} key={route} href={route} className={`w-full mr-[30px] flex justify-between px-3 py-2 mt-2 rounded-3xl ${pathname == route ? 'bg-green-50':''}`}>
                         <p>{label}</p>
                         <div className="w-[20px] h-auto">
                         <Icon icon='ArrowR' />
@@ -33,7 +51,7 @@ export default function NavResponsive() {
                     </Link>
                     ))
                     }
-                    <Link href='/contacto' className="w-full mr-[30px] flex justify-between px-6 py-4">
+                    <Link href='/contacto' className="w-full mr-[30px] flex justify-between px-3 py-2 mt-2 rounded-3xl">
                         <p>Contacto</p>
                         <div className="w-[20px] h-auto">
                         <Icon icon='ArrowR' />
