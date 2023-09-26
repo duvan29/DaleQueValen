46b088b72f9e6eca92b9d908a30d89fb0b65135b
Candeb <candebustos084@gmail.com>
2023-09-26 11:51:26 -0300

    listo mock de tipos de donar

--- src/mock/mockTiposDeDonar.js
+++ src/mock/mockTiposDeDonar.js
@@ -1,36 +1,46 @@
 import { Images } from '@/assets/img';
 
+//  {
+//     id: '1',
+//     title: 'donar dinero',
+//     description:
+//       'Podes ayudarnos a alcanzar nuestros objetivos con una donación económica a través del link de pago al hacer click en ‘Donar’ o a través de una trasferencia bancaria.',
+//     boton: 'Donar',
+//     Image: Images.logos.LogoBrown,
+//   },
+
 const tiposDeDonar = [
   {
     id: '1',
-    title: 'donar dinero',
+    title: 'colaborar con equipamiento, insumos o medicamentos',
     description:
-      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
-    boton: 'Donar',
-    Image: Images.logos.LogoBrown,
+      'Tenes insumos o equipamiento que tu niño ya no usa (sillas de ruedas, medicamentos, cuello ortopédico, lo que se te ocurra) y te gustaría poder donárselo a otro niño?',
+    boton: 'Colaborar',
+    Image: Images.logos.LogoPurple,
   },
   {
     id: '2',
     title: 'ser voluntario',
     description:
-      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
+      'Si tenes ganas de colaborar con tu tiempo, hay muchas formas de hacerlo como voluntario (redes sociales, tareas administrativas, ayuda física, toda idea es bienvenida)',
     boton: 'Colaborar',
     Image: Images.logos.LogoPink,
   },
+
   {
     id: '3',
-    title: 'colaborar con equipamiento, insumos o medicamentos',
+    title: 'asociarte como familia',
     description:
-      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
-    boton: 'Colaborar',
-    Image: Images.logos.LogoPurple,
+      'Creemos que caminar acompañado siempre es mejor. Invitamos a familias de niños con PC a unirse a nuestra comunidad.',
+    boton: 'Asociarme',
+    Image: Images.logos.LogoBlackWhite,
   },
   {
     id: '4',
-    title: 'asociarte como familia',
+    title: 'colaborar en nuestras redes',
     description:
-      'Somos Dale que Valen, una fundación que cree en el poder de la salud para mejorar la vida de los niños y niñas con parálisis cerebral',
-    boton: 'Asociarme',
-    Image: Images.logos.LogoBlackWhite,
+      'Seguinos en redes sociales, compartí nuestro contenido y ayudanos a llegar a más familias que nos necesitan.',
+    boton: 'Colaborar',
+    Image: Images.logos.LogoBrown,
   },
 ];
