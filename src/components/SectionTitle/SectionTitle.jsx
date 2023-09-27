736cebbad5a14031c96862442927d75afaa7051f
Candeb <candebustos084@gmail.com>
2023-09-27 15:44:02 -0300

    titulo seccion centrado responsive

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,10 +1,10 @@
 export default function SectionTitle({ bgColor, title }) {
   const backgroundColor = `bg-${bgColor}`;
   return (
-    <h1
-      className={`${backgroundColor} text-center text-white border lg:mt-[130px] ms:mt-[78px] lg:-ml-[140px] lg:-mr-[140px] ms:-ml-[40px] ms:-mr-[15px] mb-[15px] h-16 flex justify-center items-center `}
+    <div
+      className={`${backgroundColor} lg:mt-[130px] ms:mt-[78px] lg:-ml-[140px] lg:-mr-[140px] ms:-ml-[40px] ms:-mr-[15px] mb-[15px] h-16 flex justify-center items-center `}
     >
-      {title}
-    </h1>
+      <h1 className="text-center text-white ms:-mr-[30px] lg:mr-0">{title}</h1>
+    </div>
   );
 }
