30a85f287833c2c1c0b1f2bbfd00e7f17d64405a
Agustin <agustinperea5@gmail.com>
2023-09-28 23:13:00 -0300

    Arreglado espacio de banner sumate

--- src/app/sumate/page.jsx
+++ src/app/sumate/page.jsx
@@ -7,9 +7,7 @@ export default function Sumate() {
   return (
     <main>
       <SectionTitle bgColor="purple" title="sumate" />
-      <div className="h-[700px] flex justify-center items-center text-center">
-        <BannerSumate />
-      </div>
+      <BannerSumate />
       <SectionDonarDinero/>
       <CallToAction  bgColor="purple"/>
     </main>
