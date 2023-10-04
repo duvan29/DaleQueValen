9dad4cfe016257be31b1e188c26e0215131c3b3a
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-04 13:51:25 -0500

    Cambios Todo bien

--- src/components/Card/CardDonarSmall.jsx
+++ src/components/Card/CardDonarSmall.jsx
@@ -11,7 +11,7 @@ export default function CardDonarSmall({
   return (
     <div className={`md:w-full lg:w-[33%] ${flexGrow}`}>
       <div
-        className={` md:w-full lg:flex-col md:mb-3 md:my-8 bg-purple-30 rounded-2xl lg:justify-between lg:py-10 lg:px-10 ${flexDirection} lg:h-full lg:m-0 `}
+        className={`flex md:w-full h-full lg:flex-col md:mb-3 md:my-8 bg-purple-30 rounded-2xl lg:justify-between lg:py-10 lg:px-10 ${flexDirection} lg:h-full lg:m-0 `}
         style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}
       >
         <Image
@@ -19,17 +19,17 @@ export default function CardDonarSmall({
           className={` ${width} md:w-[30%] lg:w-full lg:min-h-[312px] lg:max-h-[350px]  rounded-2xl object-cover lg:mx-auto lg:h-[50%] `}
         />
         <div
-          className={`flex flex-col ms:justify-between md:items-center md:justify-center md:pr-20 md:py-4 md:w-full md:w-[50%] lg:w-full lg:m-0 px-4 py-3 md:py-8 lg:py-4 md:pr-16 md:pl-12 lg:p-0 lg:h-[60%] lg:justify-between lg:mb-4`}
+          className={`flex flex-col  ms:justify-between ms:h-full md:items-center md:justify-center md:pr-20 md:py-4 md:w-full md:w-[50%] lg:w-full lg:m-0 px-4 py-3 md:py-8 lg:py-4 md:pr-16 md:pl-12 lg:p-0 lg:h-[60%] lg:justify-between lg:mb-4`}
         >
-          <div className="lg:flex lg:flex-col lg:h-full">
-            <h2 className="text-center md:mt-2 ms:pb-4 text-black text-[12px] md:text-[24px] md:text-purple md:uppercase capitalize ">
-              {tipoDeDonar.title}
-            </h2>
-            <h4 className="text-[10px] md:text-[20px] md:text-left md:pt-4 lg:leading-7">
-              {tipoDeDonar.description}
-            </h4>{' '}
+          <div className="flex ms:justify-between ms:flex-col ms:h-full lg:flex-col h-full">
+              <h2 className="text-center md:mt-2 ms:pb-4 text-black text-[12px] md:text-[24px] md:text-purple md:uppercase capitalize ">
+                {tipoDeDonar.title}
+              </h2>
+              <h4 className="text-[10px] md:text-[20px] md:text-left md:pt-4 lg:leading-7">
+                {tipoDeDonar.description}
+              </h4>{' '}
             <div
-              className={`${button} text-right md:mt-1 lg:flex md:items-end lg:items-center w-full`}
+              className={`${button} lg:h-full mb-0 text-right md:mt-1 lg:flex md:items-end w-full`}
             >
               <ButtonDonar href={tipoDeDonar.href} action={tipoDeDonar.boton} />
             </div>
--- src/components/Containers/ContainerTiposDonar.jsx
+++ src/components/Containers/ContainerTiposDonar.jsx
@@ -3,15 +3,17 @@ import CardDonarSmall from '../Card/CardDonarSmall';
 
 export default function ContainerTiposDonar({ tiposDeDonar }) {
   const widthFull = 'w-full ms:h-[90px] md:h-auto';
-  const width40 = 'w-[40%]';
-  const flexGrow1 = `grow md:grow-0 `;
+  const width40 = 'w-[100%]';
+  const flexGrow1 = `grow md:grow-1`;
   const flexGrow0 = `grow w-[135px]`;
   const flexDirectionRow = `flex`;
-  const flexDirectionCol = `flex-col md:flex md:flex-row `;
+  const flexDirectionCol = `flex-col md:flex md:flex-row`;
   const centerButton = `flex ms:justify-center md:justify-end lg:justify-center `;
   const endButton = `flex ms:justify-end lg:justify-center `;
+
+
   return (
-    <div className="flex flex-wrap justify-between mt-4 gap-4 md:gap-0 items-center md:flex-nowrap md:flex-col md:justify-evenly lg:items-stretch lg:flex-row lg:gap-8 mb-8 lg:mb-20">
+    <div className="flex flex-wrap items-stretch justify-between mt-4 gap-4 md:gap-0 md:flex-nowrap md:flex-col md:justify-evenly lg:flex-row lg:gap-8 mb-8 lg:mb-20">
       {tiposDeDonar
         .filter((tipoDeDonar) => tipoDeDonar.id >= 3 && tipoDeDonar.id <= 5)
         .map((tipoDeDonar) => (
