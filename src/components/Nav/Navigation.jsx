dcd96bc4b915b8f5c0ac344a35724bb6d4c122bb
EstebanGaMi <stevegaray1234@gmail.com>
2023-10-05 14:15:51 +0100

    ultimos detalles

--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from "react";
 import Link from "next/link";
@@ -8,23 +8,28 @@ import { Images } from "@/assets/img";
 import ReusableButton from "@/components/ReusableButton/ReusableButton";
 import links from "@/mock/mockNavLinks";
 
-
 export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <div className={`${pathname=='/' ? 'pr-[40px] pl-[40px] absolute left-0 right-0':'fixed top-0 left-0 right-0 z-50 bg-white-50 backdrop-blur-sm px-[45px]'} hidden lg:block `}>
+    <div
+      className={`${
+        pathname == "/"
+          ? "pr-[40px] pl-[40px] absolute left-0 right-0"
+          : "fixed top-0 left-0 right-0 z-50 bg-white-50 backdrop-blur-sm px-[45px]"
+      } hidden lg:block `}
+    >
       <header className="flex items-center justify-between">
         <Link
           href="/"
           className={`${
-            pathname == '/' ? 'w-[370px] ml-[45px]' : 'w-[185px]'
+            pathname == "/" ? "w-[370px] ml-[45px]" : "w-[185px]"
           } h-auto`}
         >
           <Image
             className="w-full h-full aspect-ratio-16/9"
             src={
-              pathname == '/' ? Images.logos.LogoWhite : Images.logos.LogoGreen
+              pathname == "/" ? Images.logos.LogoWhite : Images.logos.LogoGreen
             }
             alt="DaleQueValen Logo"
             priority
@@ -35,15 +40,12 @@ export default function Navigation() {
             {links.map(({ label, route }) => (
               <Link key={route} href={route} className="w-[175px] mr-[30px]">
                 <ReusableButton isActive={pathname == route}>
-                {label}
+                  {label}
                 </ReusableButton>
               </Link>
-            ))
-            }
-            <Link href="#contacto" className="w-[175px]" >
-              <ReusableButton>
-                Contacto
-              </ReusableButton>
+            ))}
+            <Link href="#contacto" className="w-[175px]">
+              <ReusableButton>Contacto</ReusableButton>
             </Link>
           </div>
         </nav>
