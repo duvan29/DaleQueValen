0aac95778a2a67e0d2c64ba21debe510bec84216
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-06 11:21:24 -0500

    Cambios

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'w0.peakpx.com'
-        ], // Agrega aquí los dominios permitidos para las imágenes
-      },
 }
 
 module.exports = nextConfig
